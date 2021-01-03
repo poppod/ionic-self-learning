d08bdfbb0622c2f73e3d8fe48d7a612344a1bb69
poppod <31207266+poppod@users.noreply.github.com>
2021-01-03 17:10:03 +0700

    add routing

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
 import { PersonInputComponent } from './persons/person-input.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.mudule';
 
 @NgModule({
   declarations: [
     AppComponent,PersonsComponent,PersonInputComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
